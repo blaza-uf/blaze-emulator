ecf5aab73f7b3eb805a38dfcf4e94515fe7e7225
lia-gonz <58484338+lia-gonz@users.noreply.github.com>
2023-09-22 19:43:01 -0400

    Added execute function and modified reset
    Added execute function and integrated MemRam into the execute and reset functions.

--- src/core/CPU.cpp
+++ src/core/CPU.cpp
@@ -1,6 +1,6 @@
 #include "CPU.h"
 
-void CPU::reset() {
+void CPU::reset(MemRam &memory) {
     PC = 0xFFFC; // need to load w/contents of reset vector
     DBR = PBR = 0x00;
     X = Y = 0x00;
@@ -8,5 +8,15 @@ void CPU::reset() {
 
     f.d = 0;
     f.m = f.x = f.i = f.c = f.e = 1;
+    memory.mem_init();
 }
 
+void CPU::execute(u32 cycles, MemRam &memory) {
+    while(cycles > 0) {
+        Byte instruction = memory[PC];
+        PC++;
+        cycles--;
+
+        // need to handle instructions
+    }
+}
