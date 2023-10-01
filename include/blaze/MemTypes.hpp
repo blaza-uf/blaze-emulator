d6d76c8e4948794c78bca2f08c114c966ddb54c7
weiss-ben <weissb@ufl.edu>
2023-10-01 18:47:15 +0300

    updated formatting

--- include/blaze/MemTypes.hpp
+++ include/blaze/MemTypes.hpp
@@ -1,13 +1,13 @@
 /*
--   Memory Types for the MOS 6502 architecture
+-   Memory Types
 */
-
 #pragma once
 
 #include <cstdint>
 #include <limits>
 
-namespace Blaze {
+namespace Blaze 
+{
 	using Byte = uint8_t;
 	using Word = uint16_t;
 	using Address = uint32_t;
