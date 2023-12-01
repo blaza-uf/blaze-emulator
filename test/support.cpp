ab7c5a058aae0b880c4c10d87944ad4a6cc1fb14
Ariel Abreu <facekapow@outlook.com>
2023-11-30 21:01:04 -0500

    Define the debugging functions in tests as noops

---
+++ test/support.cpp
@@ -0,0 +1,13 @@
+#include <blaze/debug.hpp>
+
+void Blaze::clear() {
+	// noop
+};
+
+void Blaze::print(const std::string& subsystem, const std::string& message) {
+	// noop
+};
+
+void Blaze::printLine(const std::string& subsystem, const std::string& message) {
+	// noop
+};
